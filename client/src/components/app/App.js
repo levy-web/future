0770346f814a79e9a39650e728b373e7b041100f
levy <lnjiru43@gmail.com>
2023-05-11 15:59:44 +0300

    popups render

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -16,16 +16,24 @@ import Footer from '../Footer';
 import ProductByProtectedArea from '../products/ProductByProtectedArea';
 import AddProd from '../admin/AddProd';
 import Admin from '../admin/Admin';
-import Prodact from '../admin/Product';
+import Prodact from '../admin/Prodact';
 import AddProductColor from '../admin/AddProductColor';
 import BackDropColor from '../admin/BackDropColor';
+import AddFeatures from '../admin/AddFeatures';
+import BackDropFeature from '../admin/BackDropFeature';
 
 function App() {
   const  [sideMenu , setSideMenu] = useState(false)
   const [popupColor, setPopupColor] = useState(false)
+  const [popupFeature, setPopupFeature] = useState(false)
+  
 
   const togglePopupColor = () =>{
       setPopupColor((prevState) => !prevState)
+  }
+
+  const togglePopupFeature = () =>{
+    setPopupFeature((prevState) => !prevState)
   }
   
   const toggleSideMenu = () =>{
@@ -44,13 +52,15 @@ function App() {
         <Route path='/category/:protectedArea' element={<ProductByProtectedArea/>}/>
         <Route path='/admin/:add' element={<Admin/>}/>
         <Route path='/admin' element={<Admin/>}/>
-        <Route path='/admin/product/:id' element={<Prodact popColor={togglePopupColor}/>}/>
+        <Route path='/admin/product/:id' element={<Prodact popFeature={togglePopupFeature} popColor={togglePopupColor}/>}/>
       </Routes>
 
-      <BackDropColor popupColor={popupColor} popColor={togglePopupColor}/>      
+      <BackDropColor popupColor={popupColor} popColor={togglePopupColor}/>
+      <BackDropFeature popFeature={togglePopupFeature} popupFeature={popupFeature}/>
       <Backdrop sideMenu={sideMenu} closeSideMenu={toggleSideMenu}/>
-      <Menu sideMenu={sideMenu}/>
-      <AddProductColor popupColor={popupColor}/>
+      <Menu closeSideMenu={toggleSideMenu} sideMenu={sideMenu}/>
+      <AddProductColor popColor={togglePopupColor} popupColor={popupColor}/>
+      <AddFeatures popFeature={togglePopupFeature} popupFeature={popupFeature}/>
     
     </Provider>
 
