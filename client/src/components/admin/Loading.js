d6fd3114a4195a36865461da2b05fbc16ed2196c
levy <lnjiru43@gmail.com>
2023-05-12 14:33:01 +0300

    loading component

---
+++ client/src/components/admin/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='container justify-content-center'>
+        <p>Loading ...</p>
+    </div>
+  )
+}
+
+export default Loading
