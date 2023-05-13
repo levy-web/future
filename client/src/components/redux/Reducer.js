849750e3bc36a58b01e84d121eb8f03a2107fee2
levy <lnjiru43@gmail.com>
2023-05-13 13:29:08 +0300

    only persist user reducer

--- client/src/components/redux/Reducer.js
+++ client/src/components/redux/Reducer.js
@@ -1,14 +1,11 @@
-import { combineReducers } from "redux";
 import categoriesSlice from "./categories/CategorySlice";
 import featureSlice from "./features/FeatureSlice";
 import productSlice from "./product/ProductSlice";
-import userSlice from './user/UserSlice'
 
-const rootReducer = combineReducers({
+const rootReducer = {
     categories: categoriesSlice,
     products: productSlice,
     features: featureSlice,
-    user: userSlice
-});
+};
 
 export default rootReducer;
--- client/src/components/redux/Store.js
+++ client/src/components/redux/Store.js
@@ -1,19 +1,30 @@
 import {createStore, applyMiddleware} from 'redux'
+import { combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from './Reducer';
+import userSlice from './user/UserSlice';
+import categoriesSlice from './categories/CategorySlice';
+import featureSlice from './features/FeatureSlice';
+import productSlice from './product/ProductSlice';
+
 
 const persistConfig = {
-    key: 'persist-key',
+    key: 'persist-user-key',
     storage
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedUserReducer = persistReducer(persistConfig, userSlice)
+const rootReducer = combineReducers({
+    user: persistedUserReducer,
+    categories: categoriesSlice,
+    products: productSlice,
+    features: featureSlice
+})
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
-const store = createStore(persistedReducer, composedEnhancer)
+const store = createStore(rootReducer, composedEnhancer)
 const persistor = persistStore(store)
 
 export default store
