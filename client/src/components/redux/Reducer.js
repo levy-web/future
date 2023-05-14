cd99a34c2e6902220f350e912f4789209b1e000e
levy <lnjiru43@gmail.com>
2023-05-14 12:37:51 +0300

    used combine reducer

--- client/src/components/redux/Reducer.js
+++ client/src/components/redux/Reducer.js
@@ -1,14 +1,15 @@
+import { combineReducers } from "redux";
 import categoriesSlice from "./categories/CategorySlice";
 import featureSlice from "./features/FeatureSlice";
 import productSlice from "./product/ProductSlice";
 import userSlice from './user/UserSlice';
 
-const rootReducer = {
+const rootReducer = combineReducers({
     categories: categoriesSlice,
     products: productSlice,
     features: featureSlice,
     user:userSlice
 
-};
+});
 
 export default rootReducer;
--- client/src/components/redux/Store.js
+++ client/src/components/redux/Store.js
@@ -1,11 +1,9 @@
 import {createStore, applyMiddleware} from 'redux'
-import { combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from './Reducer';
-
 
 const persistConfig = {
     key: 'persist-key',
