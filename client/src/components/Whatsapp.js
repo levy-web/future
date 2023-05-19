ba64688ce303c1916f77aad710d3a92ce06f325b
levy <lnjiru43@gmail.com>
2023-05-19 09:15:33 +0300

    whatsapp link

--- client/src/components/Whatsapp.js
+++ client/src/components/Whatsapp.js
@@ -3,8 +3,8 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsAppButton = () => {
     const openWhatsApp = () => {
-      const phoneNumber = '+254 791716946'; // Replace with your phone number
-      const message = 'Hello, welcome to Sams solutions. how may I help you?'; // Replace with your default message
+      const phoneNumber = '+254 716 085 629'; // Replace with your phone number
+      const message = 'Hello, Sams solutions.'; // Replace with your default message
       const encodedMessage = encodeURIComponent(message);
       const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
       window.open(whatsappUrl, '_blank');
