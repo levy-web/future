9d83344fee4c2b591db7f2c62d2ed5cb3b47699a
Levy <lnjiru43@gmail.com>
2023-10-26 18:01:39 +0300

    changed seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -45,4 +45,83 @@ Color.create(name:"black")
 Color.create(name:"white")
 
 
+prodOne = Product.create(
+    name:"Safety Ventilated Helmet With Textile Ratchet Suspension", 
+    weight:11 , 
+    dimensions: "57.5 × 46.5 × 41 in",
+    material: "Polyethylene (HDPE)",
+    origin: "China",
+    standards: "ANSI/ISEA Z89.1-2009, EN 397:2012, CE",
+    category_id: 2 
+)
+
+prodTwo = Product.create(
+    name:"DuPont Nomex Fire Retardant Coverall", 
+    weight:30 , 
+    dimensions: "s63 × 40 × 34 in",
+    material: "Inherent FR",
+    origin: "China",
+    standards: "CE CAT 1, EN ISO 1149, EN ISO 11611, EN ISO 11612, EN ISO 14116",
+    category_id: 1 
+)
+
+prodFour = Product.create(
+    name:"Full Body Harness Integrated With Waist Comfort Belt", 
+    weight:14 , 
+    dimensions: "40 × 28 × 34 in",
+    material: "Polyethylene (HDPE)",
+    origin: "China",
+    standards: "CE 0194, EN 358:1999, EN 361:2002, EN 420",
+    category_id: 6
+)
+
+prodThree = Product.create(
+    name:"S1P SRC, Low Ankle Suede Leather Shoe", 
+    weight:20 , 
+    dimensions: "61 × 44 × 33 in",
+    material: "Genuine Leather",
+    origin: "China",
+    standards: "",
+    category_id: 8
+)
+
+prodOne.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+prodTwo.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+prodThree.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+prodFour.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+
+one = ProductColor.create(product_id:1, color_id:2)
+two = ProductColor.create(product_id:1, color_id:1)
+three= ProductColor.create(product_id:1, color_id:3)
+
+one.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+two.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+three.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+
+
+four= ProductColor.create(product_id:2, color_id:2)
+four.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+
+five = ProductColor.create(product_id:3, color_id:1)
+six = ProductColor.create(product_id:3, color_id:3)
+seven = ProductColor.create(product_id:3, color_id:5)
+eight = ProductColor.create(product_id:3, color_id:4)
+
+five.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+six.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+seven.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+eight.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+
+nine = ProductColor.create(product_id:4, color_id:2)
+ten = ProductColor.create(product_id:4, color_id:3)
+eleven = ProductColor.create(product_id:4, color_id:4)
+twelve = ProductColor.create(product_id:4, color_id:5)
+thirteen = ProductColor.create(product_id:4, color_id:6)
+
+nine.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+ten.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+eleven.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+twelve.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+thirteen.image.attach(io: File.open(Rails.root.join("public/helmet.jpg")), filename: "helmet.jpg")
+
 puts ('completed seeding')
